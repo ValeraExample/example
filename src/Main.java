bd9ab53824398382dcfca1170bd0ac861bd6d274
Кавенькин Валерий <v.kavenkin@tinkoff.ru>
2023-12-22 14:45:58 +0300

    task-2: добавлен метод

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    void blalbla() {
+        System.out.println(235);
+    }
 }
